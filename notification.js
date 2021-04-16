5d187db302545054d0c014647a4f9ce9e428db69
Martin Kattner <martin.kattner@gmail.com>
2021-04-16 08:55:11 +0200

    First version with test page

--- notification.js
+++ notification.js
@@ -1,139 +1,104 @@
-const Wait = (msg) => {
-  $("#popup-container").remove();
+const Wait = async (msg) => {
+  $("#popup-container").remove(); // just to be sure
   $("body").append(() => {
     return $("<div>").attr({
-      id: "popup-container"
-    })
-  }).append(() => {
-    return $('<div class="popup">').append(() => {
-      return $('<div class = "content">').text(msg)
-    })
+        id: "popup-container"
+      })
+      .append(() => {
+        return $('<div class="popup">').append(() => {
+          return $('<div class = "content">').text(msg)
+        })
+      })
   })
 };
 
-const Prompt = (msg) => {
+const Prompt = async (msg) => {
   return new Promise((resolve, reject) => {
-    $("#popup-container").remove();
+    $("#popup-container").remove(); // just to be sure
     $("body").append(() => {
       return $("<div>").attr({
-        id: "popup-container"
-      })
-    }).append(() => {
-      return $('<div class="popup">').append(() => {
-        return $('<div class = "content">').append(() => {
-          return $("<p>").text(msg)
-        }).append(() => {
-          return $("<input>").attr({
-            type: "text"
-          }).on("input", function() {
-            //alert()
-            var search = FUSE.search($(this).val())
-
-            $("#promptOutput #0").data("id", search[0].item.id).text(search[0].item.Name)
-            $("#promptOutput #1").data("id", search[1].item.id).text(search[1].item.Name)
-            $("#promptOutput #2").data("id", search[2].item.id).text(search[2].item.Name)
-
-          })
-        }).append(() => {
-          return $("<div>").attr({
-            id: "promptOutput"
-          }).append(() => {
-            return $("<a>").attr({
-              id: "0",
-              href: "#"
-            }).on("click", function() {
-              let id = $(this).data("id");
-              $(".popup").remove();
-              resolve(id);
+          id: "popup-container"
+        })
+        .append(() => {
+          return $('<div class="popup">').append(() => {
+            return $('<div class = "content">').append(() => {
+              return $("<p>").text(msg)
+            }).append(() => {
+              return $("<input>").attr({
+                id: "popup-input",
+                type: "text"
+              })
             })
           }).append(() => {
-            return $("<a>").attr({
-              id: "1",
-              href: "#"
-            }).on("click", function() {
-              let id = $(this).data("id");
-              $(".popup").remove();
-              resolve(id);
-            })
-          }).append(() => {
-            return $("<a>").attr({
-              id: "2",
-              href: "#"
-            }).on("click", function() {
-              let id = $(this).data("id");
-              $(".popup").remove();
-              resolve(id);
+            return $('<div class="buttons">').append(() => {
+              return $('<a class="button">').text("Cancel").on("click", () => {
+                $("#popup-container").remove();;
+                reject();
+              });
+            }).append(() => {
+              return $('<a class="button">').text("Ok").on("click", () => {
+                let val = $("#popup-input").val();
+
+                $("#popup-container").remove();;
+                resolve(val);
+              })
             })
           })
         })
-      }).append(() => {
-        return $('<div class="buttons">').append(() => {
-          return $('<a class="button">').text("Abbrechen").on("click", () => {
-            $(".popup").remove();
-            reject();
-          });
-        }).append(() => {
-          return $('<a class="button">').text("Ok").on("click", () => {
-            let id = $("#promptOutput #0").data("id");
-
-            $(".popup").remove();
-            resolve(id);
-          })
-        })
-      })
+      $(".popup input[type=text]").focus();
     })
-    $(".popup input[type=text]").focus();
   })
 
 };
 
 
-const Alert = (msg) => {
+const Alert = async (msg) => {
   return new Promise((resolve, reject) => {
-    $("#popup-container").remove();
+    $("#popup-container").remove(); // just to be sure
     $("body").append(() => {
       return $("<div>").attr({
         id: "popup-container"
-      })
-    }).append(() => {
-      return $('<div class="popup">').append(() => {
-        return $('<div class = "content">').text(msg)
+
       }).append(() => {
-        return $('<div class="buttons">').append(() => {
-          return $('<a class="button">').text("Ok").on("click", () => {
-            $(".popup").remove();
-            resolve();
-          });
+        return $('<div class="popup">').append(() => {
+          return $('<div class = "content">').text(msg)
+        }).append(() => {
+          return $('<div class="buttons">').append(() => {
+            return $('<a class="button">').text("Ok").on("click", () => {
+              $("#popup-container").remove();;
+              resolve();
+            });
+          })
         })
       })
     })
   })
 }
 
-const Confirm = (msg) => {
+const Confirm = async (msg) => {
   return new Promise((resolve, reject) => {
-    $("#popup-container").remove();
+    $("#popup-container").remove(); // just to be sure
     $("body").append(() => {
       return $("<div>").attr({
         id: "popup-container"
-      })
-    }).append(() => {
-      return $('<div class="popup">').append(() => {
-        return $('<div class = "content">').text(msg)
+
       }).append(() => {
-        return $('<div class="buttons">').append(() => {
-          return $('<a class="button">').text("Ja").on("click", () => {
-            $(".popup").remove();
-            resolve();
-          });
+        return $('<div class="popup">').append(() => {
+          return $('<div class = "content">').text(msg)
         }).append(() => {
-          return $('<a class="button">').text("Nein").on("click", () => {
-            $(".popup").remove();
-            reject();
+          return $('<div class="buttons">').append(() => {
+            return $('<a class="button">').text("Yes").on("click", () => {
+              $("#popup-container").remove();
+              resolve();
+            });
+          }).append(() => {
+            return $('<a class="button">').text("No").on("click", () => {
+              $("#popup-container").remove();
+              reject();
+            })
           })
         })
       })
     })
   })
-
 };
